55593601e0fb2998ff635d3e8efeb5389133a6ce
Adsavin <adsavin@live.com>
2017-08-26 21:08:06 +0700

    no message

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,7 +22,7 @@
 <?php $this->beginBody() ?>
 <div class="wrapper">
     <!-- Navbar-->
-    <header class="main-header hidden-print"><a class="logo" href="index.html">Vali</a>
+    <header class="main-header hidden-print"><a class="logo" href="index.php">Vali</a>
         <nav class="navbar navbar-static-top">
             <!-- Sidebar toggle button--><a class="sidebar-toggle" href="#" data-toggle="offcanvas"></a>
             <!-- Navbar Right Menu-->
@@ -67,7 +67,7 @@
             </div>
             <!-- Sidebar Menu-->
             <ul class="sidebar-menu">
-                <li class="active"><a href="index.html"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
+                <li class="active"><a href="index.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
                 <li class="treeview"><a href="#"><i class="fa fa-laptop"></i><span>UI Elements</span><i class="fa fa-angle-right"></i></a>
                     <ul class="treeview-menu">
                         <li><a href="bootstrap-components.html"><i class="fa fa-circle-o"></i> Bootstrap Elements</a></li>
