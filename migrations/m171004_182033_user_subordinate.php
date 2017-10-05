bf4eb96d979295659ff794db3d916bdf64130ef6
kovveth@gmail.com <kovveth@gmail.com>
2017-10-05 17:30:01 +0700

    fix migrate file

--- migrations/m171004_182033_user_subordinate.php
+++ migrations/m171004_182033_user_subordinate.php
@@ -8,10 +8,7 @@ class m171004_182033_user_subordinate extends Migration {
     			CREATE TABLE `user_subordinate` (
     			`user_id` int(11) NOT NULL,
     			`subordinate_user_id` int(11) NOT NULL,
-    			PRIMARY KEY (`user_id`,`subordinate_user_id`),
-    			KEY `fk_user_has_user_user2_idx` (`subordinate_user_id`),
-    			KEY `fk_user_has_user_user1_idx` (`user_id`),
-    			CONSTRAINT `fk_user_has_user_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    			PRIMARY KEY (`user_id`,`subordinate_user_id`)    			
     			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 	
            set foreign_key_checks=1;
