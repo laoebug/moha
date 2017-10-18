98fd66126e4629c94f5ba3624093acc4737731dd
Adsavin <adsavin@live.com>
2017-10-18 21:31:06 +0700

    no message

--- controllers/StatOfficerAddController.php
+++ controllers/StatOfficerAddController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\components\MyHelper;
+use app\models\Attachment;
+use app\models\Menu;
 use app\models\PhiscalYear;
 use app\models\StatOfficerAddDetail;
 use Codeception\Util\HttpCode;
@@ -19,21 +21,6 @@ use app\services\AuthenticationService;
  */
 class StatOfficerAddController extends Controller
 {
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
-
     /**
      * Lists all StatOfficerAdd models.
      * @return mixed
@@ -201,22 +188,126 @@ class StatOfficerAddController extends Controller
         ]);
     }
 
-    /**
-     * Finds the StatOfficerAdd model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return StatOfficerAdd the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
+    public function actionUpload($year)
     {
-        if (($model = StatOfficerAdd::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
+        $year = PhiscalYear::findOne($year);
+        if (!isset($year)) {
+            MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Incorrect Phiscal Year'));
+            return;
+        }
+
+        $post = Yii::$app->request->post();
+        if (!isset($post)) {
+            MyHelper::response(HttpCode::METHOD_NOT_ALLOWED, Yii::t('app', 'Incorrect Request'));
+            return;
+        }
+
+        if (!isset($_FILES['file_upload'])) {
+            MyHelper::response(HttpCode::METHOD_NOT_ALLOWED, Yii::t('app', 'Incorrect Request'));
+            return;
+        }
+
+        $menu = Menu::find()->where(['table_name' => 'stat_officer_add'])->one();
+        if (!isset($menu)) {
+            MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Data Not Found'));
+            return;
+        }
+
+        $dir = 'upload/';
+        if (!is_dir($dir)) mkdir($dir);
+        $dir .= date('Y');
+        if (!is_dir($dir)) mkdir($dir);
+
+        $tmp_name = $_FILES['file_upload']['tmp_name'];
+        $name = $_FILES['file_upload']['name'];
+        $names = explode(".", $name);
+        $ext = end($names);
+        $filename = $menu->table_name . "_" . date('Y_m_d_His') . '.' . $ext;
+
+        if (!move_uploaded_file($tmp_name, $dir . "/" . $filename)) {
+            MyHelper::response(HttpCode::INTERNAL_SERVER_ERROR, "ພົບບັນຫາໃນການອັບໂຫຼດຟາຍ");
+            return;
+        }
+
+        $model = new Attachment();
+        $model->phiscal_year_id = $year->id;
+        $model->menu_id = $menu->id;
+        $model->user_id = Yii::$app->user->id;
+        $model->deleted = 0;
+        $model->name = $filename;
+        $model->issued_no = $post['issued_no'];
+        $model->issued_date = MyHelper::convertdatefordb($post['issued_date']);
+        $model->issued_by = $post['issued_by'];
+        $model->upload_date = date('Y-m-d H:i:s');
+        $model->original_name = $name;
+        $model->dir = date('Y');
+        if (!$model->save()) {
+            unlink($dir . "/" . $filename);
+            MyHelper::response(HttpCode::INTERNAL_SERVER_ERROR, json_encode($model->errors));
+            return;
         }
     }
-    
+
+    public function actionGetreferences($year)
+    {
+        $year = PhiscalYear::findOne($year);
+        if (!isset($year)) {
+            MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Incorrect Phiscal Year'));
+            return;
+        }
+
+        $files = Attachment::find()->alias('a')
+            ->join('join', 'menu m', 'm.id = a.menu_id and m.table_name=:table', [
+                ':table' => 'stat_officer_add'
+            ])
+            ->where(['a.deleted' => 0, 'a.phiscal_year_id' => $year->id])
+            ->orderBy('upload_date desc')
+            ->asArray()->all();
+
+        return json_encode([
+            'files' => $files
+        ]);
+    }
+
+    public function actionDeletefile($year)
+    {
+        $year = PhiscalYear::findOne($year);
+        if (!isset($year)) {
+            MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Incorrect Phiscal Year'));
+            return;
+        }
+        if ($year->status != 'O') {
+            MyHelper::response(HttpCode::METHOD_NOT_ALLOWED, "The year is not allow to input");
+            return;
+        }
+        $post = Yii::$app->request->post();
+        if (isset($post)) {
+            $model = Attachment::findOne($post['id']);
+            if (!isset($model)) {
+                MyHelper::response(HttpCode::NOT_FOUND, "Data not found");
+                return;
+            }
+            $model->deleted = 1;
+            echo 'upload/' . $model->dir . '/' . $model->name;
+            if (!is_dir('upload/' . $model->dir . '/backup')) mkdir('upload/' . $model->dir . '/backup');
+
+            if (!copy('upload/' . $model->dir . '/' . $model->name, 'upload/' . $model->dir . '/backup/' . $model->name)) {
+                MyHelper::response(HttpCode::INTERNAL_SERVER_ERROR, "Cannot move file");
+                return;
+            }
+
+            if (!unlink('upload/' . $model->dir . '/' . $model->name)) {
+                MyHelper::response(HttpCode::INTERNAL_SERVER_ERROR, "Cannot delete file");
+                return;
+            }
+
+            if (!$model->save()) {
+                MyHelper::response(HttpCode::INTERNAL_SERVER_ERROR, json_encode($model->errors));
+                return;
+            }
+        }
+    }
+
     public function beforeAction($action) {
     	$user = Yii::$app->user->identity;
     	$this->enableCsrfValidation = true;
