6ce8a9d9a3293dcbf81dfada9f4344f46a9f349b
kovveth@gmail.com <kovveth@gmail.com>
2017-10-19 13:06:15 +0700

    no message

--- controllers/StatOfficerTechnicalController.php
+++ controllers/StatOfficerTechnicalController.php
@@ -10,6 +10,7 @@ use Codeception\Util\HttpCode;
 use Yii;
 use app\models\StatOfficerTechnical;
 use app\models\StatOfficerTechnicalSearch;
+use app\models\Attachment;
 use yii\db\Exception;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
