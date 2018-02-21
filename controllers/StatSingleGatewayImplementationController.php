de55c780e3fd2fc3f721ce9ee8a305e26c6a93b3
Adsavin <adsavin@live.com>
2018-02-21 23:17:22 +0700

    fix single gateway

--- controllers/StatSingleGatewayImplementationController.php
+++ controllers/StatSingleGatewayImplementationController.php
@@ -4,23 +4,20 @@ namespace app\controllers;
 
 use app\components\MyHelper;
 use app\models\Attachment;
-use app\models\Branch;
 use app\models\Menu;
 use app\models\Ministry;
 use app\models\PhiscalYear;
+use app\models\StatSingleGatewayImplementation;
 use app\models\StatSingleGatewayImplementationDetail;
+use app\services\AuthenticationService;
 use Codeception\Util\HttpCode;
 use Yii;
-use app\models\StatSingleGatewayImplementation;
-use app\models\StatSingleGatewayImplementationSearch;
 use yii\db\ActiveQuery;
 use yii\db\Exception;
 use yii\web\Controller;
 use yii\web\HttpException;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 use yii\web\ServerErrorHttpException;
-use app\services\AuthenticationService;
+
 /**
  * StatSingleGatewayImplementationController implements the CRUD actions for StatSingleGatewayImplementation model.
  */
@@ -35,39 +32,40 @@ class StatSingleGatewayImplementationController extends Controller
         return $this->render('index');
     }
 
-    public function actionGet() {
-    	
-    	$user = Yii::$app->user->identity;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    			return;
-    		}
-    	}
-    	
+    public function actionGet()
+    {
+        $user = Yii::$app->user->identity;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                return;
+            }
+        }
+
         return json_encode([
             'years' => PhiscalYear::find()->where(['deleted' => 0])->asArray()->all(),
             'ministries' => Ministry::find()->where(['deleted' => 0])->orderBy('position')->asArray()->all()
         ]);
     }
 
-    public function actionEnquiry($year) {
-    	
-    	$user = Yii::$app->user->identity;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    			return;
-    		}
-    	}
-    	
-    	
+    public function actionEnquiry($year)
+    {
+
+        $user = Yii::$app->user->identity;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                return;
+            }
+        }
+
+
         $year = PhiscalYear::findOne($year);
-        if(!isset($year)) throw new HttpException(Yii::t('app', 'Inccorect Phiscal Year'));
+        if (!isset($year)) throw new HttpException(Yii::t('app', 'Inccorect Phiscal Year'));
 
 //        $model = StatSingleGatewayImplementation::find()
 //            ->alias('i')
@@ -87,12 +85,12 @@ class StatSingleGatewayImplementationController extends Controller
 //            ->one();
 
         $model = StatSingleGatewayImplementation::find()->where(['phiscal_year_id' => $year])->one();
-        if(!isset($model)) throw new HttpException(Yii::t('app', 'No Data'));
+        if (!isset($model)) throw new HttpException(Yii::t('app', 'No Data'));
 
         $models = Ministry::find()->alias('m')
             ->select('m.*,`d`.`name` as `servicename`, `d`.`remark`')
             ->addSelect([
-                'start_date'=> 'DATE_FORMAT(`start_date`, "%d-%m-%Y")',
+                'start_date' => 'DATE_FORMAT(`start_date`, "%d-%m-%Y")',
             ])
             ->join('left join', 'stat_single_gateway_implementation_detail d', 'd.ministry_id = m.id and d.stat_single_gateway_implementation_id=:id', [':id' => $model->id])
             ->where(['deleted' => 0])
@@ -103,32 +101,33 @@ class StatSingleGatewayImplementationController extends Controller
         ]);
     }
 
-    public function actionInquiry($year, $ministry) {
-    	
-    	$user = Yii::$app->user->identity;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    			return;
-    		}
-    	}
-    	
-    	
+    public function actionInquiry($year, $ministry)
+    {
+
+        $user = Yii::$app->user->identity;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                return;
+            }
+        }
+
+
         $year = PhiscalYear::findOne($year);
-        if(!isset($year)) throw new HttpException(Yii::t('app', 'Inccorect Phiscal Year'));
+        if (!isset($year)) throw new HttpException(Yii::t('app', 'Inccorect Phiscal Year'));
 
         $model = StatSingleGatewayImplementationDetail::find()
             ->alias('d')
-            ->join('join','stat_single_gateway_implementation i', 'd.stat_single_gateway_implementation_id=i.id and i.phiscal_year_id=:year', [
+            ->join('join', 'stat_single_gateway_implementation i', 'd.stat_single_gateway_implementation_id=i.id and i.phiscal_year_id=:year', [
                 ':year' => $year->id
             ])
             ->where(['d.ministry_id' => $ministry])
             ->asArray()->one();
 
-        if(isset($model))
-            if(isset($model['start_date']))
+        if (isset($model))
+            if (isset($model['start_date']))
                 $model['start_date'] = MyHelper::convertdatefordisplay($model['start_date']);
 
         return json_encode([
@@ -136,26 +135,27 @@ class StatSingleGatewayImplementationController extends Controller
         ]);
     }
 
-    public function actionSave($year) {
-    	
-    	$user = Yii::$app->user->identity;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    			return;
-    		}
-    	}
-    	
+    public function actionSave($year)
+    {
+
+        $user = Yii::$app->user->identity;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                return;
+            }
+        }
+
         $post = Yii::$app->request->post();
-        if(isset($post)) {
+        if (isset($post)) {
             $year = PhiscalYear::findOne($year);
-            if(!isset($year)) {
+            if (!isset($year)) {
                 MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Inccorect Phiscal Year'));
                 return;
             }
-            if($year->status != 'O') {
+            if ($year->status != 'O') {
                 MyHelper::response(HttpCode::METHOD_NOT_ALLOWED, Yii::t('app', 'The Year is not allowed to input'));
                 return;
             }
@@ -171,7 +171,6 @@ class StatSingleGatewayImplementationController extends Controller
                 }
                 $model->last_update = date('Y-m-d H:i:s');
                 $model->saved = 1;
-                print_r($model->attributes);
                 if (!$model->save())
                     throw new ServerErrorHttpException(json_encode($model->errors));
 
@@ -184,14 +183,15 @@ class StatSingleGatewayImplementationController extends Controller
                     $detail->ministry_id = $post['Model']['ministry']['id'];
                 }
                 $detail->remark = $post['Model']['remark'];
-                $detail->start_date = date('Y-m-d', strtotime($post['Model']['start_date']));
+                if(isset($post['Model']['start_date']) && $post['Model']['start_date'] != '')
+                    $detail->start_date = date('Y-m-d', strtotime($post['Model']['start_date']));
+                else $detail->start_date = null;
                 $detail->name = $post['Model']['name'];
-                print_r($detail->attributes);
                 if (!$detail->save())
                     throw new ServerErrorHttpException(json_encode($detail->errors));
 
                 $transaction->commit();
-            }catch (Exception $exception) {
+            } catch (Exception $exception) {
                 $transaction->rollBack();
                 MyHelper::response(HttpCode::INTERNAL_SERVER_ERROR, $exception->getMessage());
                 return;
@@ -199,27 +199,28 @@ class StatSingleGatewayImplementationController extends Controller
         }
     }
 
-    public function actionPrint($year) {
-    	
-    	$user = Yii::$app->user->identity;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    			return;
-    		}
-    	}
-    	
+    public function actionPrint($year)
+    {
+
+        $user = Yii::$app->user->identity;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                return;
+            }
+        }
+
         $year = PhiscalYear::findOne($year);
-        if(!isset($year)) {
+        if (!isset($year)) {
             MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Inccorect Phiscal Year'));
             return;
         }
 
         $model = StatSingleGatewayImplementation::find()
             ->with([
-                'statSingleGatewayImplementationDetails' => function(ActiveQuery $query) {
+                'statSingleGatewayImplementationDetails' => function (ActiveQuery $query) {
                     $query->alias('d')
                         ->join('right join', 'ministry m', 'm.id=d.ministry_id')
                         ->orderBy('m.position');
@@ -228,34 +229,35 @@ class StatSingleGatewayImplementationController extends Controller
         return $this->renderPartial('print', ['content' => $this->renderPartial('table', ['model' => $model])]);
     }
 
-    public function actionDownload($year) {
-    	
-    	$user = Yii::$app->user->identity;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    			return;
-    		}
-    	}
-    	
+    public function actionDownload($year)
+    {
+
+        $user = Yii::$app->user->identity;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                return;
+            }
+        }
+
         $year = PhiscalYear::findOne($year);
-        if(!isset($year)) {
+        if (!isset($year)) {
             MyHelper::response(HttpCode::NOT_FOUND, Yii::t('app', 'Inccorect Phiscal Year'));
             return;
         }
 
         $model = StatSingleGatewayImplementation::find()
             ->with([
-                'statSingleGatewayImplementationDetails' => function(ActiveQuery $query) {
+                'statSingleGatewayImplementationDetails' => function (ActiveQuery $query) {
                     $query->alias('d')
                         ->join('right join', 'ministry m', 'm.id=d.ministry_id')
                         ->orderBy('m.position');
                 }
             ])->where(['phiscal_year_id' => $year->id])->one();
         return $this->renderPartial('excel', [
-            'file' => 'Single Gateway Implementation '.$year->year.'.xls',
+            'file' => 'Single Gateway Implementation ' . $year->year . '.xls',
             'content' => $this->renderPartial('table', ['model' => $model])
         ]);
     }
@@ -379,25 +381,26 @@ class StatSingleGatewayImplementationController extends Controller
             }
         }
     }
-    
-    public function beforeAction($action) {
-    	$user = Yii::$app->user->identity;
-    	$this->enableCsrfValidation = true;
-    	$controller_id = Yii::$app->controller->id;
-    	$acton_id = Yii::$app->controller->action->id;
-    	if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
-    		if (! AuthenticationService::isAccessibleAction ( $controller_id, $acton_id )) {
-    			if (Yii::$app->request->isAjax) {
-    				MyHelper::response ( HttpCode::UNAUTHORIZED, Yii::t ( 'app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication' ) . " with ID:  " . $controller_id . "/ " . $acton_id );
-    				return;
-    			} else {
-    				return $this->redirect ( [
-    						'authentication/notallowed'
-    				] );
-    			}
-    		}
-    	}
-    
-    	return parent::beforeAction ( $action );
+
+    public function beforeAction($action)
+    {
+        $user = Yii::$app->user->identity;
+        $this->enableCsrfValidation = true;
+        $controller_id = Yii::$app->controller->id;
+        $acton_id = Yii::$app->controller->action->id;
+        if ($user->role ["name"] != Yii::$app->params ['DEFAULT_ADMIN_ROLE']) {
+            if (!AuthenticationService::isAccessibleAction($controller_id, $acton_id)) {
+                if (Yii::$app->request->isAjax) {
+                    MyHelper::response(HttpCode::UNAUTHORIZED, Yii::t('app', 'HTTP Error 401- You are not authorized to access this operaton due to invalid authentication') . " with ID:  " . $controller_id . "/ " . $acton_id);
+                    return;
+                } else {
+                    return $this->redirect([
+                        'authentication/notallowed'
+                    ]);
+                }
+            }
+        }
+
+        return parent::beforeAction($action);
     }
 }
