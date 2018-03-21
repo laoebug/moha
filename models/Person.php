bc94b6ef951bdcbaa48103a8903df871ffdd3d04
kovveth@gmail.com <kovveth@gmail.com>
2018-03-21 18:26:23 +0700

    no message

---
+++ models/Person.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "person".
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ */
+class Person extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'person';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['first_name', 'last_name'], 'required'],
+            [['first_name', 'last_name'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'first_name' => Yii::t('app', 'First Name'),
+            'last_name' => Yii::t('app', 'Last Name'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return PersonQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new PersonQuery(get_called_class());
+    }
+}
